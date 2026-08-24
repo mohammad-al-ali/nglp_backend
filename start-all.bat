9053f3eca88b30bdbf169063b9e7ad7e9b049dd4
Mohammad Alali <m.alali@acquaintech-iq.com>
2026-08-24 15:33:49 +0300

    V5

--- start-all.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d D:\ProjectFifthYear\NGLP
-
-start "Spring Boot" cmd /k "cd backend.v1 && mvn spring-boot:run"
-start "Frontend" cmd /k "cd frontend.v1 && npm run dev"  
-start "Python Microservice" cmd /k "cd extract_transcription_microservice && python -m uvicorn server:app --port 8000"
-
-echo All services launched!
